61837e0ecc9f77ba3f1b86f68051c0ff13d11551
FADILI EL MOSTAFA <fadilimustafa530@gmail.com>
2024-02-24 16:04:57 +0100

    @fadili: implements unit test with one Test class instance using @TestInstance(TestInstance.Lifecycle.PER_CLASS)

--- src/main/java/unitTestfadili/mostafa/unitTest/service/UserService.java
+++ src/main/java/unitTestfadili/mostafa/unitTest/service/UserService.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface UserService {
     String createUser(Map<String, String> userDetails);
-    Map updateUser(String userId, Map userDetails);
-    Map getUserDetails(String userId);
+    Map<String, String> updateUser(String userId, Map<String, String> userDetails);
+    Map<String, String> getUserDetails(String userId);
     void deleteUser(String userId);
 }
--- src/test/java/unitTestfadili/mostafa/unitTest/service/UserServiceImplTest.java
+++ src/test/java/unitTestfadili/mostafa/unitTest/service/UserServiceImplTest.java
@@ -1,20 +1,49 @@
 package unitTestfadili.mostafa.unitTest.service;
 
-import org.junit.jupiter.api.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import unitTestfadili.mostafa.unitTest.io.UsersDatabase;
+import unitTestfadili.mostafa.unitTest.io.UsersDatabaseMapImpl;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceImplTest {
+	
+	private UsersDatabase usersDatabase;
+	private UserService userService;
+	private String createdUserId = "";
 
     @BeforeAll
     void setup() {
         // Create & initialize database
+    	usersDatabase = new UsersDatabaseMapImpl();
+    	usersDatabase.init();
+    	
+    	userService = new UserServiceImpl(usersDatabase);
     }
 
     @AfterAll
     void cleanup() {
         // Close connection
+    	usersDatabase.close();
         // Delete database
+    	usersDatabase = null;
+    	
+    	userService = null;
     }
 
     @Test
@@ -22,6 +51,17 @@ public class UserServiceImplTest {
     @DisplayName("Create User works")
     void testCreateUser_whenProvidedWithValidDetails_returnsUserId() {
 
+    	//given
+    	Map<String, String> user = new HashMap<>();
+    	user.put("firstName", "mostafa");
+    	user.put("lastName", "fadili");
+    	
+    	
+    	//when
+    	createdUserId = userService.createUser(user);
+    	
+    	//then
+    	assertNotNull(createdUserId);
     }
 
 
@@ -30,6 +70,20 @@ public class UserServiceImplTest {
     @DisplayName("Update user works")
     void testUpdateUser_whenProvidedWithValidDetails_returnsUpdatedUserDetails() {
 
+    	//given
+    	Map<String, String> newUser = new HashMap<>();
+    	newUser.put("firstName", "bilal");
+    	newUser.put("lastName", "fadili");
+    	
+    	//when
+    	Map<String, String> updatedUser = userService.updateUser(createdUserId, newUser);
+    	
+    	//then
+    	assertEquals(newUser.get("firstName"), updatedUser.get("firstName"),
+    			"returned value of user's firstName is incorrect");
+    	
+    	assertEquals(newUser.get("lastName"), updatedUser.get("lastName"),
+    			"returned value of user's lastName is incorrect");
     }
 
     @Test
@@ -37,6 +91,14 @@ public class UserServiceImplTest {
     @DisplayName("Find user works")
     void testGetUserDetails_whenProvidedWithValidUserId_returnsUserDetails() {
 
+    	
+    	//when
+    	Map<String, String> userDetails = userService.getUserDetails(createdUserId);
+    	
+    	//then
+    	assertNotNull(userDetails, "user shouldn't be null");
+    	assertEquals(createdUserId, userDetails.get("userId"),
+    			"returned user contains incorect user id");
     }
 
     @Test
@@ -44,6 +106,12 @@ public class UserServiceImplTest {
     @DisplayName("Delete user works")
     void testDeleteUser_whenProvidedWithValidUserId_returnsUserDetails() {
 
+    	//when
+    	userService.deleteUser(createdUserId);
+    	
+    	//then
+    	assertNull(userService.getUserDetails(createdUserId),
+    			"user should not be found");
     }
 
 }
